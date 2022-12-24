f4bcf3f907de8ed82fe8912a1c31ce9b90eb8bd2
AVINASH BHUSARI <118240956+avinashCap@users.noreply.github.com>
2022-12-24 14:29:05 +0530

    Update A.java

--- src/tcs/A.java
+++ src/tcs/A.java
@@ -2,7 +2,7 @@ package tcs;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("A");
-        System.out.println("B");
+        System.out.println("********************");
+        
     }
 }
